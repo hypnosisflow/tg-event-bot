def0fc017eb1a4f79660aaa9ae8d9bb730ffc775
Artem <plzv92@gmail.com>
2024-02-14 14:21:31 +0700

    register flow

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 // var pg = import("pg");
+import "dotenv/config.js";
 import pg from "pg";
 //or native libpq bindings
 //var pg = require('pg').native
 
-var conString =  "postgres://fhvvtxxn:BEdbIFvWMUUfOLE5p6j028V3mXbTaqg6@cornelius.db.elephantsql.com/fhvvtxxn";
- //Can be found in the Details page
+var conString = process.env.DB_STRING;
+//Can be found in the Details page
 var client = new pg.Client(conString);
 
 console.log("db connnection");
